d7665b03e040f03d88e4fa7bcaa8dcaaac4780a7
Lucas Moura <lucas.moura020@gmail.com>
2023-02-26 23:20:34 -0300

    Adição de comentários explicativos

--- Relatório 1/main.m
+++ Relatório 1/main.m
@@ -1,17 +1,19 @@
 %Problema de dois corpos
+
 %Constantes
 mi_Terra = 3.986*(10^5); %km3/s2
 
 %-----------------------------------------------------------
 
 %Resoluções
-%(item a) 2 07276 64.2707 228.5762 6489050 281.4937 16.8767 2.45097347248963
-%(item b) 2 02717 0.7404 32.0789 0014515 234.9766 357.1212 1.00360058109167
 
-%Semieixo maior
-%item a
+%Item a
+%two lines: 2 07276 64.2707 228.5762 6489050 281.4937 16.8767 2.45097347248963
+
+%Conversão da anomalia media para verdadeira
 theta_a = converter_anomalia_media_verdadeira(deg2rad(16.8767), 0.6489050);
 
+%Semieixo maior
 [semieixo_a, n_a] = semieixoMaior(mi_Terra, 2.4509);
 
 %Coordenada Orbital
@@ -29,28 +31,38 @@ velocidadeInercial_a = matrizTranspostaRotacao(deg2rad(64.2707), deg2rad(281.493
 %--------------------------------------------------------
 
 %item b
+%two lines: 2 02717 0.7404 32.0789 0014515 234.9766 357.1212 1.00360058109167
+
+%Conversão da anomalia media para verdadeira
 theta_b = converter_anomalia_media_verdadeira(deg2rad(357.1212),0.0014515);
 
+%Semieixo maior
 [semieixo_b, n_b] = semieixoMaior(mi_Terra, 1.0036);
 
+
+%Coordenada Orbital
 [xo_b, yo_b, zo_b] = dirOrbital(deg2rad(theta_b), 0.0014515, semieixo_b);
 
+%Coordenada Inercial
 coordInercial_b = matrizTranspostaRotacao(deg2rad(0.7404), deg2rad(234.9766), deg2rad(32.0789)) * [xo_b; yo_b; zo_b];
 
+%Velocidade Orbital
 [vx_b, vy_b, vz_b] = velOrbital(n_b, 0.0014515, deg2rad(theta_b), semieixo_b);
 
+%Velocidade Inercial
 velocidadeInercial_b = matrizTranspostaRotacao(deg2rad(0.7404), deg2rad(234.9766), deg2rad(32.0789)) * [vx_b; vy_b; vz_b];
 
-%-----------------------------------------------------------------
 
+
+%Aplicação do processo de integração
 %ODE 45
 
-%Item a
+%Dados do Item a
 T_a = 2*pi/n_a;
 r_a = coordInercial_a';
 v_a = velocidadeInercial_a';
 
-%Item b
+%Dados do Item b
 T_b = 2*pi/n_b;
 r_b = coordInercial_b';
 v_b = velocidadeInercial_b';
@@ -67,30 +79,39 @@ colormap(cool)
 axis equal
 hold on 
 
+%Condição inicial do item A
 InitCond = [r_a v_a];
+
+%Condição inicial do item B
 InitCond2 = [r_b v_b];
 
-[Times,Out] = ode45(@edos, [0 T_a], InitCond);
-[Times2,Out2] = ode45(@edos, [0 T_b], InitCond2);
+%Solução para o item a |(Possível modificar o multiplicar em T_a e T_b)
+[Times,Out] = ode45(@edos, [0 1*T_a], InitCond);
+
+%Solução para o item b
+[Times2,Out2] = ode45(@edos, [0 1*T_b], InitCond2);
 
 %Plot do grafico
+%Plot item a
 p = plot3(Out(:,1),Out(:,2),Out(:,3));
+%Plot item b
 p2 = plot3(Out2(:,1),Out2(:,2),Out2(:,3));
 
 axis equal
 grid on
 
-%textura da terra no elipsoide (necessaria uma conexao com a internet)
+%Textura da terra no elipsoide (Necessaria uma conexao com a internet)
 texture_url = 'http://www.shadedrelief.com/natural3/images/earth_clouds.jpg';
 cdata = flip(imread(texture_url));
 set(body, 'FaceColor', 'texturemap', 'CData', cdata, 'EdgeColor', 'none');
 
-%solucao analitica pontos
-semieixo = semieixo_a;
-semieixo2 = semieixo_b;
-ex = 0.6489050;
-ex2 = 0.0014515;
+%Solução Analítica (Geração dos pontos em vermelho)
 
+%Item A
+semieixo = semieixo_a;
+ex = 0.6489050;
+
+%Plot item A
 for theta=0:0.07:deg2rad(360)
     p = semieixo*(1 - ex^2);
     r = p/(1+ex*cos(theta));
@@ -101,6 +122,11 @@ for theta=0:0.07:deg2rad(360)
     scatter3(coordInercial(1,1), coordInercial(2,1), coordInercial(3,1), '.','red')
 end
 
+%Item B
+semieixo2 = semieixo_b;
+ex2 = 0.0014515;
+
+%Plot item B
 for theta=0:0.07:deg2rad(360)
     p = semieixo2*(1 - ex2^2);
     r = p/(1+ex2*cos(theta));
